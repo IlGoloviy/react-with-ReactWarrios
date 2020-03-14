7e1f2b315f02ab56fe669032780dc81711ccba73
Illia Holovii <goloviyillya@gmail.com>
2020-03-14 19:13:53 +0200

    connect api

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { moviesData } from '../moviesData';
+import axios from 'axios';
+import { API_URL, API_KEY_3 } from '../utils/api';
+
 import Movie from './Movie';
 
 class App extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      movies: moviesData,
+      movies: [],
       moviesWillWatch: []
     }
+  }
+
+  componentDidMount() {
+    axios.get(`${API_URL}/discover/movie?api_key=${API_KEY_3}`)
+      .then(res => {
+        this.setState({movies: res.data.results})
+      })
   }
 
   deleteMovie = data => {
@@ -34,31 +43,35 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-9">
-            <div className="row">
-              {this.state.movies.map(movie => {
-                return (
-                  <div className="col-6 mb-4" key={movie.id}>
-                    <Movie  
-                      data={movie} 
-                      deleteMovie={this.deleteMovie}
-                      addWillWatchMovie={this.addWillWatchMovie}
-                      deleteWillWatchMovie={this.deleteWillWatchMovie}
-                    ></Movie>
-                  </div>
-                )
-              })}
+    if (this.state.movies.length) {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-9">
+              <div className="row">
+                {this.state.movies.map(movie => {
+                  return (
+                    <div className="col-6 mb-4" key={movie.id}>
+                      <Movie  
+                        data={movie} 
+                        deleteMovie={this.deleteMovie}
+                        addWillWatchMovie={this.addWillWatchMovie}
+                        deleteWillWatchMovie={this.deleteWillWatchMovie}
+                      ></Movie>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+            <div className="col-3">
+              <p>Will Watch: {this.state.moviesWillWatch.length}</p>
             </div>
           </div>
-          <div className="col-3">
-            <p>Will Watch: {this.state.moviesWillWatch.length}</p>
-          </div>
-        </div>
-      </div> 
-    );
+        </div> 
+      );
+
+    }
+    return null;
   }
 }
 
