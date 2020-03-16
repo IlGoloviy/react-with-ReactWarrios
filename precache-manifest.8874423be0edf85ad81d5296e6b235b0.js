44adeaf7f70fdafd10cd5288ea461bd0ab3a1e33
Illia Holovii <goloviyillya@gmail.com>
2020-03-16 20:38:06 +0200

    Updates

---
+++ precache-manifest.8874423be0edf85ad81d5296e6b235b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14717780bb682596f0fe5b3ea0d741c",
+    "url": "/react-with-ReactWarrios/index.html"
+  },
+  {
+    "revision": "db3eddf221c6e6a50aaa",
+    "url": "/react-with-ReactWarrios/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d8902c19296da297bc88",
+    "url": "/react-with-ReactWarrios/static/css/main.1aa79601.chunk.css"
+  },
+  {
+    "revision": "db3eddf221c6e6a50aaa",
+    "url": "/react-with-ReactWarrios/static/js/2.99010e08.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-with-ReactWarrios/static/js/2.99010e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8902c19296da297bc88",
+    "url": "/react-with-ReactWarrios/static/js/main.738200ca.chunk.js"
+  },
+  {
+    "revision": "10b7c032756bb0a9a611",
+    "url": "/react-with-ReactWarrios/static/js/runtime-main.6b7e2848.js"
+  }
+]);
